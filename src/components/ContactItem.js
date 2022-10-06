0610480c57d05638c3835e5bb796abe30fa3ea87
Andi Paris Bachtiar <andiparis02@gmail.com>
2022-10-06 12:14:30 +0700

    Add delete button

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactItemImage from './ContactItemImage';
 import ContactItemBody from './ContactItemBody';
+import DeleteButton from './DeleteButton';
 
-function ContactItem({ imageUrl, name, tag }) {
+function ContactItem({ id, imageUrl, name, tag, onDelete }) {
   return (
     <div className="contact-item">
       <ContactItemImage imageUrl={imageUrl} />
       <ContactItemBody name={name} tag={name} />
+      <DeleteButton id={id} onDelete={onDelete} />
     </div>
   );
 }
