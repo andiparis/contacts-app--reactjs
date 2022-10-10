8a8962d2f03907d663a133a80238e64745f02499
Andi Paris Bachtiar <andiparis02@gmail.com>
2022-10-10 14:27:54 +0700

    Add input contact feature

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,30 +1,51 @@
 import React from 'react';
+import ContactInput from './ContactInput';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
 
 class ContactApp extends React.Component {
- constructor(props) {
-  super(props);
-  this.state = {
-    contacts: getData()
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: getData()
+    }
+
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
   }
 
-  this.onDeleteHandler = this.onDeleteHandler.bind(this);
- }
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg'
+          }
+        ]
+      };
+    });
+  }
 
- onDeleteHandler(id) {
-  const contacts = this.state.contacts.filter(contact => contact.id !== id);
-  this.setState({ contacts });
- }
+  onDeleteHandler(id) {
+    const contacts = this.state.contacts.filter(contact => contact.id !== id);
+    this.setState({ contacts });
+  }
 
- render() {
-  return (
-    <div className="contact-app">
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-    </div>
-  );  
- }
+  render() {
+    return (
+      <div className="contact-app">
+        <h1>Aplikasi Kontak</h1>
+        <h2>Tambah Kontak</h2>
+        <ContactInput addContact={this.onAddContactHandler} />
+        <h2>Daftar Kontak</h2>
+        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+      </div>
+    );  
+  }
 }
 
 export default ContactApp;
