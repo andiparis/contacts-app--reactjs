e1b7f1d7676e2bb22501af6f1755c085b7037c46
Andi Paris Bachtiar <andiparis02@gmail.com>
2022-10-16 18:56:31 +0700

    Deleted add & delete handler

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -1,51 +1,24 @@
 import React from 'react';
-import ContactInput from './ContactInput';
-import ContactList from './ContactList';
-import { getData } from '../utils/data';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './Navigation';
+import HomePage from '../pages/HomePage';
+import AddPage from '../pages/AddPage';
 
-class ContactApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: getData()
-    }
-
-    this.onAddContactHandler = this.onAddContactHandler.bind(this);
-    this.onDeleteHandler = this.onDeleteHandler.bind(this);
-  }
-
-  onAddContactHandler({ name, tag }) {
-    this.setState((prevState) => {
-      return {
-        contacts: [
-          ...prevState.contacts,
-          {
-            id: +new Date(),
-            name,
-            tag,
-            imageUrl: '/images/default.jpg'
-          }
-        ]
-      };
-    });
-  }
-
-  onDeleteHandler(id) {
-    const contacts = this.state.contacts.filter(contact => contact.id !== id);
-    this.setState({ contacts });
-  }
-
-  render() {
-    return (
-      <div className="contact-app">
+function ContactApp() {
+  return (
+    <div className="contact-app">
+      <header className="contact-app__header">
         <h1>Aplikasi Kontak</h1>
-        <h2>Tambah Kontak</h2>
-        <ContactInput addContact={this.onAddContactHandler} />
-        <h2>Daftar Kontak</h2>
-        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-      </div>
-    );  
-  }
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default ContactApp;
